1332d24797230bd8e9c7dd02ba38c0e6c62253d8
Oleksandr Kostan <kostan.law@gmail.com>
2022-09-08 17:23:26 +0300

    п

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const imageGallery = document.querySelector(".gallery");
+console.log(imageGallery);
+
+// function createColorCardsMarkup(colors) {
+//   return colors
+//     .map(({ hex, rgb }) => {
+//       return `
+//     <div class="color-card">
+//      <div><div><div> <div
+//      class="color-swatch"
+//      data-hex="${hex}"
+//      data-rgb="${rgb}"
+//      style="background-color: ${hex}"
+//    ></div></div></div></div>
+//       <div class="color-meta">
+//         <p>HEX: ${hex}</p>
+//         <p>RGB: ${rgb}</p>
+//       </div>
+//     </div>
+//     `;
+//     })
+//     .join("");
+// }
